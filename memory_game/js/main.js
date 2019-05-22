dd1911e591ea6d20e226940a940e8529b324c9f5
Kelsey Cox <kelseycox@Lucy.local>
2019-05-21 19:52:21 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,24 +22,43 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		} else {
-			console.log("Sorry, try again");
+			alert("Sorry, try again");
 		}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0);
+function resetCards() {
+	for (i = 0; i <cards.length; i++) {
+		var cardElement = document.getElementsByTagName('img')[i];
+		cardElement.setAttribute('src', "images/back.png");
+	}
+}
 
-flipCard(2);
+createBoard();
+
+document.getElementById('reset-button').addEventListener('click', resetCards);
